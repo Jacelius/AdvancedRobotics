2df4434445147a99604843c03a9f913a35b31704
ojoe <ojoe@itu.dk>
2023-08-28 15:27:26 +0200

    feat:added robot project

---
+++ MyFirstProect/main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+
+# Initialize motors
+left_motor = Motor(Port.A)
+right_motor = Motor(Port.D)
+
+robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=90)
+
+# Constants
+forward_distance = 900  
+left_distance = 450
+
+# Move forward
+robot.straight(forward_distance)
+
+# Turn left
+left_motor.run_angle(500, -180) 
+right_motor.run_angle(500, 180)  
+robot.stop()
+
+# Move left
+robot.straight(left_distance)
+
+
+left_motor.run_angle(500, 90) 
+right_motor.run_angle(500, -90) 
+robot.stop()
+
+# End of program
+# brick.sound.beep()
+
+
+
+
