143dd9376955ed0ca9afd441d3326a8a49e5b0ad
ojoe <ojoe@itu.dk>
2023-09-04 15:09:57 +0200

    feat:We lost robot race :(

--- MyFirstProect/main.py
+++ MyFirstProect/main.py
@@ -7,10 +7,56 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
 
+
+def target1():
+    forward_distance = 100.52
+    robot.turn(-26.57)
+    robot.straight(forward_distance*10)
+    robot.turn(-140)
+
+def target1Reverse():
+    forward_distance = 100.52
+    robot.turn(140)
+    robot.straight(-(forward_distance*10))
+    robot.turn(26.57)
+
+def target2():
+    forward_distance = 107
+    robot.straight(forward_distance*10)
+
+def target2Reverse():
+    forward_distance = 107
+    robot.straight(-(forward_distance*10))
+
+
+def target3():
+    forward_distance = 100
+    robot.straight(forward_distance*10)
+    robot.turn(90)
+    robot.straight(45*10)
+
+def target3Reverse():
+    robot.straight(-(45*10))
+    robot.turn(-90)
+    robot.straight(-(100*10))
+
+def flexprogram():
+    target1()
+    target1Reverse()
+    target2()
+    target2Reverse()
+    target3()
+    target3Reverse()
+
+def lightsensorRun():
+    while True:
+        if lightsensor.reflection() > 20:
+            robot.drive(100,0)
+        else:
+            robot.stop()
 
 
 # Initialize motors
@@ -18,33 +64,21 @@ left_motor = Motor(Port.A)
 right_motor = Motor(Port.D)
 
 robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=90)
+#lightsensor = ColorSensor(Port.S1)
 
 # Constants
-forward_distance = 100.62
+
 
 # Move forward
 # robot.straight(forward_distance)
 
-robot.turn(-30)
+#flexprogram()
+#lightsensorRun()
+#target1()
+#target2()
+target3()
 
-robot.straight(forward_distance*10)
-
-
-# robot.turn(-180)
-
-# Turn left
 robot.stop()
-
-# Move left
-# robot.straight(left_distance)
-
-
-# left_motor.run_angle(500, 90) 
-# right_motor.run_angle(500, -90) 
-robot.stop()
-
-# End of program
-# brick.sound.beep()
 
 
 
