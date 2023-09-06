a398d031c57bafe4610f794a0e5377704c335de7
kjaerb <kjaerb@gmail.com>
2023-09-06 16:26:39 +0200

    feat: 3 sensor

---
+++ 3_sensor_maze/main.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, ColorSensor,)
+from pybricks.parameters import Port
+from pybricks.robotics import DriveBase
+from pybricks import ev3brick as brick
+
+
+def maze_3_sensor(instruction_set):
+    while len(instruction_set) > 0:
+        if is_corner():
+            execute_instruction(instruction_set[0])
+            instruction_set.pop(0)
+            print("corner \n")
+        elif should_correct():
+            correct_robot()
+            print("correct \n")
+        else:
+            straight()
+
+    brick.sound.beep()
+
+
+def execute_instruction(instruction):
+    robot.straight(nudge_dist + 50)
+    instruction()
+    brick.sound.beep()
+
+
+def is_intersection():
+    return is_on_center_line() and is_on_left_line() and is_on_right_line()
+
+
+def is_corner():
+    if (is_on_left_line() and is_on_center_line()) or (is_on_right_line() and is_on_center_line()):
+        return True
+    else:
+        return False
+
+
+def is_on_left_line():
+    return left_light.reflection() < reflection_rate
+
+
+def is_on_right_line():
+    return right_light.reflection() < reflection_rate
+
+
+def is_on_center_line():
+    return center_light.reflection() < reflection_rate
+
+
+def should_correct():
+    return not is_on_center_line()
+
+
+def correct_robot():
+    if is_on_left_line():
+        turn_left_amount(10)
+    elif is_on_right_line():
+        turn_right_amount(10)
+    else:
+        print("going backwards \n")
+        robot.straight(10)
+        if is_on_left_line():
+            robot.straight(-5)
+            turn_left_amount(20)
+        elif is_on_right_line():
+            robot.straight(-5)
+            turn_right_amount(20)
+
+
+def straight():
+    robot.drive(speed, drive_angle)
+
+
+def turn_right_amount(degrees):
+    robot.turn(degrees)
+
+
+def turn_right():
+    robot.turn(turn_radius)
+
+
+def turn_left_amount(degrees):
+    robot.turn(-degrees)
+
+
+def turn_left():
+    robot.turn(-turn_radius)
+
+
+# Initialize motors
+left_motor = Motor(Port.A)
+right_motor = Motor(Port.D)
+
+# Initialize the lightsensors
+left_light = ColorSensor(Port.S4)
+right_light = ColorSensor(Port.S1)
+center_light = ColorSensor(Port.S2)
+
+robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=90)
+
+# Constants
+speed = 100
+drive_angle = 0
+turn_radius = 100
+angle = 5
+reflection_rate = 15
+max_num_corrections = 2
+nudge_dist = 10
+
+# Move forward
+# robot.straight(forward_distance)
+
+# flexprogram()
+# lightsensorRun()
+# target1()
+# target2()
+# target3()
+
+instructions = [turn_right, turn_left, turn_left,
+                straight, turn_right, turn_right, straight, straight, turn_right, straight, turn_right, straight]
+
+simple_instructions = [turn_right, turn_right, straight,
+                       straight, turn_right, straight, turn_right]
+
+# maze(instructions)
+
+
+maze_3_sensor(instructions)
+
+robot.stop()
--- MyFirstProect/main.py
+++ MyFirstProect/main.py
@@ -66,79 +66,99 @@ def pause():
 
 def maze(instructionset):
     while len(instructionset) > 0:
-        if shouldCorrect():
-            corrections = 0
-            while shouldCorrect():
-                correctRobot()
+        if is_intersection():
+            print("Intersection")
+            execute_instruction(instructionset[0])
+            instructionset.pop(0)
+        elif should_correct():
+            corrections, turn_state = 0, 0
+            while should_correct():
+                turn_state += correct_robot()
+                print("Turn state: " + str(turn_state))
                 corrections += 1
-                if corrections > 5:
-                    robot.straight(70)
-                    instructionset[0]()
+                if corrections > max_num_corrections:
+                    print("Is corner, turning: " + str(turn_state))
+                    robot.turn(turn_state*1.5)
+                    execute_instruction(instructionset[0])
                     instructionset.pop(0)
 
-        elif isIntersection():
-            while isIntersection():
-                straight()
-            robot.straight(10)
-            instructionset[0]()
-            instructionset.pop(0)
         else:
             straight()
 
     brick.sound.beep()
 
 
-def isIntersection():
-    if left_light.reflection() < 15 and right_light.reflection() < 15:
+def execute_instruction(instruction):
+    # while is_on_atleast_one_line():
+    #     straight()
+    robot.straight(nudge_dist + 50)
+    instruction()
+    brick.sound.beep()
+
+
+def is_on_atleast_one_line():
+    if left_light.reflection() < reflection_rate or right_light.reflection() < reflection_rate:
         return True
     else:
         return False
 
 
-def shouldCorrect():
-    if (left_light.reflection() < 15 and right_light.reflection() > 15) or (right_light.reflection() < 15 and left_light.reflection() > 15):
+def is_intersection():
+    if left_light.reflection() < reflection_rate and right_light.reflection() < reflection_rate:
         return True
     else:
         return False
 
 
-def correctRobot():
+def should_correct():
+    if (left_light.reflection() < reflection_rate and right_light.reflection() > reflection_rate) or (right_light.reflection() < reflection_rate and left_light.reflection() > reflection_rate):
+        return True
+    else:
+        return False
+
+
+def correct_robot():
     if should_turn_left():
-        robot.turn(-5)
+        robot.turn(angle)
+        return -angle
     if should_turn_right():
-        robot.turn(5)
-
-
-def isCorner():
-    pass
+        robot.turn(-angle)
+        return angle
 
 
 def should_turn_left():
-    if left_light.reflection() < 15:
+    if left_light.reflection() < reflection_rate:
         return True
     else:
         return False
 
 
 def should_turn_right():
-    if right_light.reflection() < 15:
+    if right_light.reflection() < reflection_rate:
         return True
     else:
         return False
 
 
 def straight():
-    robot.drive(100, 0)
+    robot.drive(speed, drive_angle)
 
 
 def turn_right():
-    robot.turn(75)
+    robot.turn(turn_radius)
     straight()
 
 
 def turn_left():
-    robot.turn(-75)
+    robot.turn(-turn_radius)
     straight()
+
+
+def is_on_center_line():
+    if center_light.reflection() < reflection_rate:
+        return True
+    else:
+        return False
 
 
 # Initialize motors
@@ -152,7 +172,13 @@ right_light = ColorSensor(Port.S1)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=90)
 
 # Constants
-
+speed = 100
+drive_angle = 0
+turn_radius = 90
+angle = 5
+reflection_rate = 15
+max_num_corrections = 2
+nudge_dist = 10
 
 # Move forward
 # robot.straight(forward_distance)
@@ -164,9 +190,12 @@ robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=90)
 # target3()
 
 instructions = [turn_right, turn_left, turn_left,
-                straight, turn_right, turn_right, straight, straight, turn_right, straight, turn_right]
+                straight, turn_right, turn_right, straight, straight, turn_right, straight, turn_right, straight]
 
-maze(instructions)
+simple_instructions = [turn_right, turn_right, straight,
+                       straight, turn_right, straight, turn_right]
+
+# maze(instructions)
 
 
 robot.stop()
