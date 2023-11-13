c7d6ad3b6dec996cdafbfd4b853935e75089d2f5
Silas Paaske <sipn@itu.dk>
2023-11-13 13:00:18 +0100

    testing bluefinding
    Co-authored-by: Oliver Holmberg Jørgensen <EraxterCodes@users.noreply.github.com>

--- computer_vision/CameraTest.py
+++ computer_vision/CameraTest.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def capture_and_save_image(file_path):
     # Number 0 indicates that we want to capture from the first available camera.
@@ -9,8 +10,29 @@ def capture_and_save_image(file_path):
         print("Error: Couldn't open the camera.")
         return
 
-    # Read the current frame from the video capture
-    ret, frame = cap.read()
+    width = 500
+    height = 400
+
+
+    cv2.startWindowThread()
+    blueDetected = False
+    while True:
+        ret, frame = cap.read()
+        cv2.imshow('frame', frame)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        blue_lower = np.array([90, 50, 50])
+        blue_upper = np.array([120, 255, 255])
+        mask = cv2.inRange(hsv, blue_lower, blue_upper)
+        result = cv2.bitwise_and(frame, frame, mask=mask)
+
+        cv2.imshow('Original     Image', cv2.resize(frame, (width, height)))
+        cv2.imshow('Blue Mask', cv2.resize(mask, (width, height)))
+        cv2.imshow('Result', cv2.resize(result, (width, height)))
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+        if blueDetected == True:
+            print("Blue Detected")
 
     if ret:
         # Save the image on disk
